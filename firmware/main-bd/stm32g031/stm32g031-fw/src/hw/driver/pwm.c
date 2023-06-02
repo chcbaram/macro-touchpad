98e2dc5ab5475b882346b16061af74f6d38a20c9
Hancheol Cho <chcbaram@gmail.com>
2023-06-02 23:24:41 +0900

    LCD PWM 25KHz로 변경

--- firmware/main-bd/stm32g031/stm32g031-fw/src/hw/driver/pwm.c
+++ firmware/main-bd/stm32g031/stm32g031-fw/src/hw/driver/pwm.c
@@ -42,7 +42,7 @@ bool pwmInit(void)
   __HAL_RCC_TIM14_CLK_ENABLE();
 
   htim14.Instance = TIM14;
-  htim14.Init.Prescaler = 127;
+  htim14.Init.Prescaler = 9;
   htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim14.Init.Period = 255;
   htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
